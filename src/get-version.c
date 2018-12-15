b912457d61fe527e554a1cb1ebd55c642fb736eb
KyuHyuk Lee <lee@kyuhyuk.kr>
2018-12-15 23:51:25 +0900

    Add 'get-version.c' - Import Linux version from zimage File.

--- src/get-version.c
+++ src/get-version.c
@@ -1,8 +1,8 @@
 /********************************************************************
-* FILENAME : extract-ikconfig.c
+* FILENAME : get-version.c
 *
 * DESCRIPTION :
-*       Extracting the config file from the zImage file.
+*       Import Linux version from zimage File.
 *
 * AUTHOR : KyuHyuk Lee <lee@kyuhyuk.kr>   START DATE : 15 Dec 2018
 *
@@ -14,24 +14,22 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 #define KERNEL_FILENAME "zImage"
-#define KERNEL_CONFIG_FILENAME "config.gz"
 
-const unsigned char startPatterns[] = {0x49,0x4b,0x43,0x46,0x47,0x5f,0x53,0x54};
-const unsigned char endPatterns[] = {0x49,0x4b,0x43,0x46,0x47,0x5f,0x45,0x44};
+const unsigned char startPatterns[] = {0x4C,0x69,0x6E,0x75,0x78,0x20,0x76,0x65,0x72,0x73,0x69,0x6F,0x6E};
+const unsigned char endPatterns[] = {0x0A,0x00};
 
 unsigned long int getStartOffset(unsigned char *buffer, unsigned int inputFileSize);
 unsigned long int getEndOffset(unsigned long int startOffset, unsigned char *buffer, unsigned int inputFileSize);
 
 int main() {
-        FILE *fp;
+        FILE *fp = fopen(KERNEL_FILENAME, "r");;
         unsigned char *buffer;
-        unsigned char *configFile;
+        unsigned char *version;
         unsigned int count;
         unsigned int inputFileSize;
-        unsigned int configFileSize;
+        unsigned int versionLen;
         unsigned long int startOffset, endOffset;
 
-        fp  = fopen(KERNEL_FILENAME, "r");
         fseek(fp, 0, SEEK_END);
         inputFileSize = ftell(fp);
         buffer = malloc(inputFileSize + 1);
@@ -41,18 +39,15 @@ int main() {
         printf("Filename : %s, Size: %d\n", KERNEL_FILENAME, inputFileSize);
         fclose(fp);
 
-        startOffset = getStartOffset(buffer, inputFileSize) + 8;
+        startOffset = getStartOffset(buffer, inputFileSize);
         endOffset = getEndOffset(startOffset, buffer, inputFileSize);
-        configFileSize = endOffset - startOffset;
-        printf("config.gz file size : %d\n", configFileSize);
+        versionLen = endOffset - startOffset;
 
-        fp = fopen (KERNEL_CONFIG_FILENAME, "w");
-        configFile = malloc(configFileSize + 1);
-        memset(configFile, 0, configFileSize + 1);
-        memcpy(configFile, buffer + startOffset, configFileSize + 1 );
-        fwrite(configFile, configFileSize, 1, fp);
-        fclose(fp);
-        free(configFile);
+        version = malloc(versionLen + 1);
+        memset(version, 0, versionLen + 1);
+        memcpy(version, buffer + startOffset, versionLen + 1 );
+        printf("%s\n", version);
+        free(version);
         return 0;
 }
 
@@ -66,7 +61,12 @@ unsigned long int getStartOffset(unsigned char *buffer, unsigned int inputFileSi
                    buffer[i+4] == startPatterns[4] &&
                    buffer[i+5] == startPatterns[5] &&
                    buffer[i+6] == startPatterns[6] &&
-                   buffer[i+7] == startPatterns[7] ) {
+                   buffer[i+7] == startPatterns[7] &&
+                   buffer[i+8] == startPatterns[8] &&
+                   buffer[i+9] == startPatterns[9] &&
+                   buffer[i+10] == startPatterns[10] &&
+                   buffer[i+11] == startPatterns[11] &&
+                   buffer[i+12] == startPatterns[12] ) {
                         printf("Start Offset : 0x%X\n", i + 8);
                         return i;
                 }
@@ -79,13 +79,7 @@ unsigned long int getEndOffset(unsigned long int startOffset, unsigned char *buf
         int i = 0;
         for(i=startOffset; i<inputFileSize; i++) {
                 if(buffer[i] == endPatterns[0] &&
-                   buffer[i+1] == endPatterns[1] &&
-                   buffer[i+2] == endPatterns[2] &&
-                   buffer[i+3] == endPatterns[3] &&
-                   buffer[i+4] == endPatterns[4] &&
-                   buffer[i+5] == endPatterns[5] &&
-                   buffer[i+6] == endPatterns[6] &&
-                   buffer[i+7] == endPatterns[7] ) {
+                   buffer[i+1] == endPatterns[1] ) {
                         printf("End Offset : 0x%X\n", i);
                         return i;
                 }
